adc0e613867cb352f9c0d80b21ee42f97ee1a344
Alexandre Junior <jasj_jr@hotmail.com>
2019-06-02 22:55:33 -0300

    Configuring mongoDB and mongoose

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-rocketseat-gonode-modulo-3
+# Module 3
+
+Marketplace app developed from 3rd Node.js module classes.
+
+## Frameworks and packages
+
+Here are some frameworks and packages used in this project:
+
+API
+
+-   Express
+
+Database
+
+-   mongoose
+-   MongoDB (docker container)
+
+Development
+
+-   Nodemon
+-   ESLint
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
+
+class App {
+    constructor () {
+        this.express = express()
+        this.isDev = process.env.NODE_ENV !== 'production'
+
+        this.middlewares()
+        this.routes()
+    }
+
+    database () {
+        mongoose.connect(databaseConfig.uri, {
+            useCreateIndex: true,
+            useNewUrlParser: true
+        })
+    }
+
+    middlewares () {
+        this.express.use(express.json)
+    }
+
+    routes () {
+        this.express.use(routes)
+    }
+}
+
+module.exports = new App().express
